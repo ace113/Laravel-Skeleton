16f660dc971c39186d990323783f9be172b3453c
prajwalmaharjan58@gmail.com <prajwalmaharjan58@gmail.com>
2021-11-23 10:58:33 +0545

    small fix

--- public/js/general.js
+++ public/js/general.js
@@ -28,22 +28,28 @@ var permissionChecks = document.getElementsByName('permission[]'),
     permissionSelectAll = document.querySelector('.per_select_all'),
     permissionDeselectAll = document.querySelector('.per_deselect_all')
 
-function perSelectAll() {
-    for (var checkbox of permissionChecks) {
-        //    console.log(checkbox)
-        checkbox.setAttribute('checked', true);
-        checkbox.parentElement.classList.add('checked')
+if(permissionChecks.length> 0){
+    if(permissionChecks){
+        function perSelectAll() {
+            for (var checkbox of permissionChecks) {
+                //    console.log(checkbox)
+                checkbox.setAttribute('checked', true);
+                checkbox.parentElement.classList.add('checked')
+            }
+        }
+        
+        permissionSelectAll.addEventListener('click', perSelectAll);
+        
+        function perDeselectAll() {
+            for (var checkbox of permissionChecks) {
+                //    console.log(checkbox)
+                checkbox.removeAttribute('checked', true);
+                checkbox.parentElement.classList.remove('checked')
+            }
+        }
+        
+        permissionDeselectAll.addEventListener('click', perDeselectAll);
     }
 }
 
-permissionSelectAll.addEventListener('click', perSelectAll);
 
-function perDeselectAll() {
-    for (var checkbox of permissionChecks) {
-        //    console.log(checkbox)
-        checkbox.removeAttribute('checked', true);
-        checkbox.parentElement.classList.remove('checked')
-    }
-}
-
-permissionDeselectAll.addEventListener('click', perDeselectAll);
